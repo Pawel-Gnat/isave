ffb961d812029180c5a080ec3fd8ba5280e52064
Pawel Gnat <pawel_gnat@o2.pl>
2024-07-17 21:20:26 +0200

    chore: add logo and favicon

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { signOut } from 'next-auth/react';
 
 import { Button } from '@/components/ui/button';
@@ -31,8 +32,8 @@ const PAGES = [
 const Navbar = () => {
   return (
     <nav className="sticky flex h-screen flex-col items-center gap-4 p-6">
-      <Link href="/" className="hidden p-2 lg:block">
-        iSave
+      <Link href="/" className="hidden pb-2 text-5xl font-bold text-primary lg:block">
+        <Image src="/isave.svg" alt="iSave" width={60} height={60} />
       </Link>
 
       <ul className="mt-16 flex flex-col gap-4 text-left">
