bc4ec2bca899efe27cf50ead085888665e038d04
Pawel Gnat <pawel_gnat@o2.pl>
2024-07-19 19:19:35 +0200

    build: prisma binaryTargets

--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,5 +1,5 @@
 export const Banner = () => {
-  // if (process.env.NEXT_PUBLIC_ENV === 'production') return;
+  if (process.env.NEXT_PUBLIC_ENV === 'production') return;
 
   return (
     <div className="fixed bottom-0 right-0 flex flex-col p-4 text-right text-destructive">
