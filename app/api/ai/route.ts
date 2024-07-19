28e6d3ee3feb39ccb93c0d5f9de96223f98132fc
Pawel Gnat <pawel_gnat@o2.pl>
2024-07-19 19:03:15 +0200

    ci/cd: add public env

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -15,6 +15,10 @@ interface CompletionResponse {
   }[];
 }
 
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
+
 export async function POST(request: Request) {
   const body = await request.json();
   const { fileText } = body;
@@ -32,10 +36,6 @@ export async function POST(request: Request) {
       { status: 405 },
     );
   }
-
-  const openai = new OpenAI({
-    apiKey: process.env.API_KEY,
-  });
 
   const expenseCategories = await getExpenseCategories();
   const simplifiedCategories = simplifyCategories(expenseCategories);
