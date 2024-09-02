476bfd750ab47fc603c6ec45e476277eb567f3e9
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-02 20:25:16 +0200

    chore: layout import format

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,7 +1,7 @@
+import { ReactNode } from 'react';
 import { redirect } from 'next/navigation';
 
 import getCurrentUser from '@/actions/getCurrentUser';
-import { ReactNode } from 'react';
 
 export default async function AuthLayout({ children }: { children: ReactNode }) {
   const user = await getCurrentUser();
