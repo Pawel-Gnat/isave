f7d820c03be154199e308100287eab8716748c87
Pawel Gnat <pawel_gnat@o2.pl>
2024-11-21 18:41:15 +0100

    chore: bump month

--- app/(root)/(routes)/components/group-container.tsx
+++ app/(root)/(routes)/components/group-container.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { endOfMonth, startOfMonth, subMonths } from 'date-fns';
+import { endOfMonth, startOfMonth } from 'date-fns';
 
 import useGroupBudgetsStatistics from '@/hooks/useGroupBudgetsStatistics';
 
@@ -10,10 +10,7 @@ import { GroupBudgetCard } from './group-budget-card';
 
 export const GroupContainer = () => {
   const { groupBudgetsStatistics, isGroupBudgetsStatisticsLoading } =
-    useGroupBudgetsStatistics(
-      startOfMonth(subMonths(new Date(), 1)),
-      endOfMonth(subMonths(new Date(), 1)),
-    );
+    useGroupBudgetsStatistics(startOfMonth(new Date()), endOfMonth(new Date()));
 
   if (isGroupBudgetsStatisticsLoading) {
     return (
